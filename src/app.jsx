0548d4379a61c1f848417d1532ab73809d900ed0
tnsska2 <vnemftnsska2@naver.com>
2021-10-19 00:47:20 +0900

    State / Props / Ref

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,67 @@
 import './app.css';
-import Habit from './components/habit';
+import Habits from './components/habits';
 
-function App() {
-  return <Habit />;
+import React, { Component } from 'react';
+import Navbar from './components/navbar';
+import HabitAddForm from './components/habitAddForm';
+
+class app extends Component {
+  state = {
+    habits: [
+      { id: 1, name: 'Reading', count: 0 },
+      { id: 2, name: 'Running', count: 0 },
+      { id: 3, name: 'Coding', count: 0 },
+    ],
+  };
+
+  handleIncreament = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits });
+    // this.setState({ habits: habits });
+  };
+
+  handleDecreament = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    if (habits[index].count > 0) {
+      habits[index].count--;
+    }
+    this.setState({ habits });
+  };
+
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter(item => item.id !== habit.id);
+    this.setState({ habits });
+  };
+
+  handleAdd = name => {
+    const habits = [...this.state.habits, { id: Date.now(), name, count: 0 }];
+    this.setState({ habits });
+  };
+
+  handleReset = () => {
+
+  };
+
+  render() {
+    return (
+      <>
+        <Navbar
+          totalCount={this.state.habits.filter(item => item.count > 0).length}
+        />
+        <Habits
+          habits={this.state.habits}
+          onIncreament={this.handleIncreament}
+          onDecreament={this.handleDecreament}
+          onDelete={this.handleDelete}
+          onAdd={this.handleAdd}
+          onReset={this.handleReset}
+        />
+      </>
+    );
+  }
 }
 
-export default App;
+export default app;
--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
+  handleIncreament = () => {
+    this.props.onIncreament(this.props.habit);
+  };
+
+  handleDecreament = () => {
+    this.props.onDecreament(this.props.habit);
+  };
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
+
+  };
+
   render() {
+    const { name, count } = this.props.habit;
     return (
       <>
         <li className="habit">
-          <span className="habit-name">Reading</span>
-          <span className="habit-count">8</span>
-          <button className="habit-button habit-increase">
+          <span className="habit-name">{name}</span>
+          <span className="habit-count">{count}</span>
+          <button
+            className="habit-button habit-increase"
+            onClick={this.handleIncreament}
+          >
             <i className="fas fa-plus"></i>
           </button>
-          <button className="habit-button habit-decrease">
+          <button
+            className="habit-button habit-decrease"
+            onClick={this.handleDecreament}
+          >
             <i className="fas fa-minus"></i>
           </button>
-          <button className="habit-button habit-delete">
+          <button
+            className="habit-button habit-delete"
+            onClick={this.handleDelete}
+          >
             <i className="fas fa-trash"></i>
           </button>
         </li>
